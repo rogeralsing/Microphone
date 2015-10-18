9356fa13a7d2f31c0b3e4d6124920b2b48589f12
rogeralsing <rogeralsing@gmail.com>
2015-10-18 13:15:47 +0200

    Built in reapers

--- Microphone.Core/Cluster.cs
+++ Microphone.Core/Cluster.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Consul;
 
 namespace Microphone.Core
@@ -48,6 +50,49 @@ namespace Microphone.Core
                     Interval = TimeSpan.FromSeconds(1),
                     TTL = TimeSpan.Zero,
                     Timeout = TimeSpan.Zero
+                }
+            });
+            StartReaper();
+        }
+
+        private static void StartReaper()
+        {
+            Task.Factory.StartNew(async () =>
+            {
+                await Task.Delay(1000).ConfigureAwait(false);
+                Logger.Information("Reaper started..");
+                var client = new Client();
+                var lookup = new HashSet<string>();
+                while (true)
+                {
+                    try
+                    {
+                        var checks = client.Agent.Checks();
+                        foreach (var check in checks.Response)
+                        {
+                            if (Equals(check.Value.Status, CheckStatus.Critical))
+                            {
+                                //dont delete new services
+                                if (lookup.Contains(check.Value.ServiceID))
+                                {
+                                    client.Agent.ServiceDeregister(check.Value.ServiceID);
+                                    Logger.Information("Unregistering {ServiceId}", check.Value.ServiceID);
+                                }
+                                else
+                                {
+                                    Logger.Information("Marking {ServiceId} as up for reaping", check.Value.ServiceID);
+                                    lookup.Add(check.Value.ServiceID);
+                                }
+
+                            }
+                        }
+                    }
+                    catch(Exception x)
+                    {
+                        Logger.Error(x,"Crashed");
+                    }
+
+                    await Task.Delay(5000).ConfigureAwait(false);
                 }
             });
         }
