3e5529760d10ff66f0475cafe193c7ea05282978
rogeralsing <rogeralsing@gmail.com>
2015-10-18 08:14:19 +0200

    nugetpacking

---
+++ Microphone.Core/build.bat
@@ -0,0 +1,2 @@
+nuget pack Microphone.Core.csproj -IncludeReferencedProjects -Prop Configuration=Release
+nuget push Microphone.Core.0.1.0.0.nupkg
--- Microphone.Nancy/Properties/AssemblyInfo.cs
+++ Microphone.Nancy/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NancyBotstrapper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Microphone.Nancy")]
+[assembly: AssemblyDescription("Microservice framework using NancyFx and Consul")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NancyBotstrapper")]
+[assembly: AssemblyCompany("Roger Johansson")]
+[assembly: AssemblyProduct("Microphone.Nancy")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
