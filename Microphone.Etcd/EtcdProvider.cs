5affd399335fbfc2fc437ad35bcaa61ae1f708c7
rogeralsing <rogeralsing@gmail.com>
2015-10-20 23:46:40 +0200

    beginning etcd provider

--- Microphone.Etcd/EtcdProvider.cs
+++ Microphone.Etcd/EtcdProvider.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Net.Http;
 using Microphone.Core;
 using Microphone.Core.ClusterProviders;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Microphone.Etcd
 {
@@ -13,12 +16,28 @@ namespace Microphone.Etcd
 
         public void RegisterService(string serviceName, string serviceId, string version, Uri uri)
         {
-            throw new NotImplementedException();
+            RegisterServiceAsync(serviceName, serviceId, version, uri).Wait();
+        }
+
+        public async Task RegisterServiceAsync(string serviceName, string serviceId, string version, Uri uri)
+        {
+            var url = $"http://127.0.0.1:2379/v2/keys/services/{serviceName}/{serviceId}";
+            var client = new HttpClient();
+            var payload = new
+            {
+                value = uri.ToString(),
+                xyz = "1123",
+                ttl = "1000"
+            };
+            var json = JsonConvert.SerializeObject(payload);
+            var content = new StringContent(json);
+            var response = await client.PutAsync(url, content).ConfigureAwait(false);
+
         }
 
         public string GetConfig()
         {
-            throw new NotImplementedException();
-        }
+            return "";
+        }       
     }
 }
