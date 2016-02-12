bb98c678ff6b119ac5db5407415b8dc1268a1602
Steven Atkinson <steven.atkinson@gmx.com>
2016-02-12 12:30:34 +0000

    Added new base class so we can specify the bootstrapper we want to use for the NancyProvider.

--- Microphone.Nancy/NancyProvider.cs
+++ Microphone.Nancy/NancyProvider.cs
@@ -6,45 +6,49 @@ using Nancy.Hosting.Self;
 
 namespace Microphone.Nancy
 {
-    public class NancyProvider : IFrameworkProvider
+	public class NancyProvider<TBootstrapper> : IFrameworkProvider where TBootstrapper : DefaultNancyBootstrapper
+	{
+		public Uri Start(string serviceName, string version)
+		{            
+			var uri = Configuration.GetUri();
+			var conf = GetConfiguration();
+			var host = GetHost(uri, conf);            
+			return uri;
+		}
+
+		private static NancyHost GetHost(Uri uri, HostConfiguration hostConfigs)
+		{
+			while (true)
+			{
+				try
+				{
+					var bootstrapper = Activator.CreateInstance<TBootstrapper>();
+					var host = new NancyHost(uri, bootstrapper, hostConfigs);
+					host.Start();
+					return host;
+				}
+				catch
+				{
+					Task.Delay(1000).Wait();
+					Console.WriteLine("Port allocation failed, retrying.");
+				}
+			}
+		}
+
+		private static HostConfiguration GetConfiguration()
+		{
+			var hostConfigs = new HostConfiguration
+			{
+				UrlReservations =
+				{
+					CreateAutomatically = true
+				}
+				};
+			return hostConfigs;
+		}
+	}
+
+	public class NancyProvider : NancyProvider<DefaultNancyBootstrapper>
     {
-
-        public Uri Start(string serviceName, string version)
-        {            
-            var uri = Configuration.GetUri();
-            var conf = GetConfiguration();
-            var host = GetHost(uri, conf);            
-            return uri;
-        }
-
-        private static NancyHost GetHost(Uri uri, HostConfiguration hostConfigs)
-        {
-            while (true)
-            {
-                try
-                {
-                    var host = new NancyHost(uri, new DefaultNancyBootstrapper(), hostConfigs);
-                    host.Start();
-                    return host;
-                }
-                catch
-                {
-                    Task.Delay(1000).Wait();
-                    Console.WriteLine("Port allocation failed, retrying.");
-                }
-            }
-        }
-
-        private static HostConfiguration GetConfiguration()
-        {
-            var hostConfigs = new HostConfiguration
-            {
-                UrlReservations =
-                {
-                    CreateAutomatically = true
-                }
-            };
-            return hostConfigs;
-        }
     }
 }
