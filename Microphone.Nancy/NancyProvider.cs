ef12efa3f3e252fce30361da4289fd9d32eafe28
Steven Atkinson <steven.atkinson@gmx.com>
2016-02-12 16:02:17 +0000

    Swapped tabs for spaces.

--- Microphone.Nancy/NancyProvider.cs
+++ Microphone.Nancy/NancyProvider.cs
@@ -6,49 +6,49 @@ using Nancy.Hosting.Self;
 
 namespace Microphone.Nancy
 {
-	public class NancyProvider<TBootstrapper> : IFrameworkProvider where TBootstrapper : DefaultNancyBootstrapper
-	{
-		public Uri Start(string serviceName, string version)
-		{            
-			var uri = Configuration.GetUri();
-			var conf = GetConfiguration();
-			var host = GetHost(uri, conf);            
-			return uri;
-		}
+  public class NancyProvider<TBootstrapper> : IFrameworkProvider where TBootstrapper : DefaultNancyBootstrapper
+  {
+    public Uri Start(string serviceName, string version)
+    {
+      var uri = Configuration.GetUri();
+      var conf = GetConfiguration();
+      var host = GetHost(uri, conf);
+      return uri;
+    }
 
-		private static NancyHost GetHost(Uri uri, HostConfiguration hostConfigs)
-		{
-			while (true)
-			{
-				try
-				{
-					var bootstrapper = Activator.CreateInstance<TBootstrapper>();
-					var host = new NancyHost(uri, bootstrapper, hostConfigs);
-					host.Start();
-					return host;
-				}
-				catch
-				{
-					Task.Delay(1000).Wait();
-					Console.WriteLine("Port allocation failed, retrying.");
-				}
-			}
-		}
+    private static NancyHost GetHost(Uri uri, HostConfiguration hostConfigs)
+    {
+      while (true)
+      {
+        try
+        {
+          var bootstrapper = Activator.CreateInstance<TBootstrapper>();
+          var host = new NancyHost(uri, bootstrapper, hostConfigs);
+          host.Start();
+          return host;
+        }
+        catch
+        {
+          Task.Delay(1000).Wait();
+          Console.WriteLine("Port allocation failed, retrying.");
+        }
+      }
+    }
 
-		private static HostConfiguration GetConfiguration()
-		{
-			var hostConfigs = new HostConfiguration
-			{
-				UrlReservations =
-				{
-					CreateAutomatically = true
-				}
-				};
-			return hostConfigs;
-		}
-	}
+    private static HostConfiguration GetConfiguration()
+    {
+      var hostConfigs = new HostConfiguration
+      {
+        UrlReservations =
+        {
+          CreateAutomatically = true
+        }
+        };
+      return hostConfigs;
+    }
+  }
 
-	public class NancyProvider : NancyProvider<DefaultNancyBootstrapper>
+  public class NancyProvider : NancyProvider<DefaultNancyBootstrapper>
     {
     }
 }
