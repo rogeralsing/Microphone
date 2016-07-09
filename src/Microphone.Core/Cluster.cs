4f16027ec00faa1761abbea793bef5c1d863a04b
rogeralsing <rogeralsing@gmail.com>
2016-07-09 09:12:57 +0200

    more logging

--- src/Microphone.Core/Cluster.cs
+++ src/Microphone.Core/Cluster.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microphone.Core.ClusterProviders;
 using static Microphone.Core.Logger;
 using Microsoft.Extensions.Logging;
