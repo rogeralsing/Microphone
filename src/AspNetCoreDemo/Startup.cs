ac7e600fcb016a9f32245c3faab7200d5e31c1b2
rogeralsing <rogeralsing@gmail.com>
2016-07-11 22:19:48 +0200

    fsharp

--- src/AspNetCoreDemo/Startup.cs
+++ src/AspNetCoreDemo/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
---
+++ src/Microphone.FSharp/FSharp.fs
@@ -0,0 +1,7 @@
+﻿namespace Microphone
+module FSharp =
+
+    let resolveService (name: string) = Cluster.Client.ResolveService name
+    
+    let registerService (name: string) (version: string) (url: System.Uri) (cluster: IClusterProvider) = 
+        Cluster.RegisterService({ new IFrameworkProvider with member x.GetUri() = url },cluster,name,version,null)
