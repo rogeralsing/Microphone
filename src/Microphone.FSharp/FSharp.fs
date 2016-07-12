5998fb13657b033dcb2384080273660759dbb355
rogeralsing <rogeralsing@gmail.com>
2016-07-12 18:30:00 +0200

    451

--- src/Microphone.FSharp/FSharp.fs
+++ src/Microphone.FSharp/FSharp.fs
@@ -3,9 +3,8 @@ module FSharp =
     open Suave
 
     let resolveService (name: string) = 
-        let res = Cluster.Client.ResolveService name
-        (res.Host,res.Port)
-    
+        let res = Cluster.Client.ResolveUrl name
+        
     let registerService (name: string) (version: string) (cluster: IClusterProvider) (url: System.Uri) =
         let framework = { new IFrameworkProvider with member x.GetUri() = url } 
         Cluster.RegisterService(framework,cluster,name,version,null)
